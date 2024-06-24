6ea41ab47840108420aca5c1389234d095fd37ba
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2024-06-24 15:18:31 -0300

    Adding first calculator

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, jsonify, request
+from src.calculators.calculator_1 import Calculator1
 
 calc_route_bp = Blueprint('calc_routes', __name__)
 
 @calc_route_bp.route('/calculator/1', methods=['POST'])
 def calculator_1():
+  calc = Calculator1()
+  calc.calculate(request)
+  
   print(request.json)
   return jsonify({ 'success': True }), 200
